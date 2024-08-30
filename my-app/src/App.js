dafadd83d823b491b3ddb6e8780e25dedf307113
Ninganna <arunningu@gmail.com>
2024-08-30 23:41:09 +0530

    components and files added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Banner from './Components/Banner'
+// import Header from './Components/Header'
+import Body from './Components/Body'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <Header/>
+        <Banner/>
+        <Body/>
+        <Footer/>
+    </div>
   )
 }
 
